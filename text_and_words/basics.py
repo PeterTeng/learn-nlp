2ababfade8ccb38f7cb2c14ec5cc4f699a718ba1
DONGSHIH TENG <tou9702@gmail.com>
2017-07-08 23:46:27 +0900

    Sample of Concordance

---
+++ text_and_words/basics.py
@@ -0,0 +1,9 @@
+import nltk
+# Need to download books data with nltk.download()
+from nltk.book import *
+
+#
+# Search text
+# ------------------------------
+# Concordance: search text with words around it
+print(text1.concordance("monstrous"))
