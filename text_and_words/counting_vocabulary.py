17c7e0ddff45dafa3c503b1a2696129f48ee528c
DONGSHIH TENG <tou9702@gmail.com>
2017-07-09 21:17:55 +0900

    Lexical diversity

--- text_and_words/counting_vocabulary.py
+++ text_and_words/counting_vocabulary.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import nltk
 from nltk.book import *
 
@@ -7,3 +8,14 @@ print(len(text3))
 # Distinct  words
 print(sorted(set(text3)))
 print(len(set(text3)))
+
+# Lexical Diversity
+def lexical_diversity(text):
+    return len(text) / len(set(text))
+
+# Percentage of Lexical Diversity
+def percentage(count, total):
+    return 100 * count / total
+
+print(lexical_diversity(text3))
+print(percentage(text4.count('a'), len(text4)))
